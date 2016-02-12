be56d066b405b1295b4f2db96f981d550e78ce90
jihyuns <carpedie7720@gmail.com>
2016-02-12 06:15:54 +0000

    change keyboard note

--- piano/piano.py
+++ piano/piano.py
@@ -4,7 +4,7 @@ from minimix.quick import *
 
 wav_file = toWav('./sounds/pianoc.ogg')
 
-sf_folder = shifts(wav_file, range(-10, 10))
+sf_folder = shifts(wav_file, range(-16, 16))
 
 cf_file = cf(sf_folder, 'keyboards/pianoc.kb')
 
