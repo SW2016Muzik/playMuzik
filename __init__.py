9bceb9aa12f2b79ed52894fe185ff1e1c987178d
jihyuns <carpedie7720@gmail.com>
2016-02-16 05:14:49 +0000

    Retest

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+Minimix is a minimalistic implementation of a mixer or instrumnet
+emulator played using your computer's keyboard.
+Functions :
+	minimix('myConf.conf'):main function.
+"""
+
+from minimix import minimix
--- violin/violin.py
+++ violin/violin.py
@@ -2,10 +2,10 @@ from minimix import minimix
 raw_input('')
 from minimix.quick import *
 
-wav_file = toWav('./sounds/pianoc.ogg')
+wav_file = toWav('./sounds/violin.ogg')
 
 sf_folder = shifts(wav_file, range(-16, 16))
 
-cf_file = cf(sf_folder, 'keyboards/pianoc.kb')
+cf_file = cf(sf_folder, 'keyboards/violin.kb')
 
 minimix(cf_file)
